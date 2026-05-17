146776cac56b253ce576de96b566233c209e1417
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-05-17 20:34:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (16.34%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int n = sqrt(num);
+        if(n*n == num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
