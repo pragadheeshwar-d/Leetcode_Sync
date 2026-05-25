df78ad4f58e0984e190906fc29cf0a5a9f24b91f
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-05-25 17:48:33 +0530

    Time: 3 ms (25.74%), Space: 33.1 MB (24.7%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int>ans;
+        for(int i=0;i<words.size();i++){
+            if(count(words[i].begin(),words[i].end(),x))
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
