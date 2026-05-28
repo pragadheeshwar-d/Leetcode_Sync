0b7f23e5aef1b1f9cddc7bdcecb71412697f2bf7
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-05-28 14:42:21 +0530

    Time: 52 ms (44.59%), Space: 56.1 MB (83.75%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    let P = init;
+
+    function increment() {
+        return ++P;
+    }
+
+    function decrement() {
+        return --P;
+    }
+
+    function reset() {
+        return (P = init);
+    }
+
+    return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
