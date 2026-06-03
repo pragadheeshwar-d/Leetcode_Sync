c40b67a94554fa80261979800e0302b95a8a93d7
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-06-03 12:15:11 +0530

    Time: 2 ms (79.82%), Space: 237.9 MB (92.66%) - LeetHub

---
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int cal(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d) {
+        int mini = 2e9, ans = 2e9;
+        for (int i = 0; i < a.size(); i++) mini = min(mini, a[i] + b[i]);
+        for (int i = 0; i < c.size(); i++) ans = min(ans, max(mini, c[i]) + d[i]);
+        return ans;
+    }
+    int earliestFinishTime(vector<int>& lS, vector<int>& lD, vector<int>& wS, vector<int>& wD) {
+        return min(cal(lS, lD, wS, wD), cal(wS, wD, lS, lD));
+    }
+};
