226b06b5ed388f27f9c8596210e46029e4947d78
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-06-26 21:13:04 +0530

    Time: 92 ms (14.07%), Space: 66.9 MB (10.17%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,26 +1,61 @@
+#include <vector>
+
 class Solution {
 public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
+    int countMajoritySubarrays(std::vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> f(2 * n + 1, 0);
-        f[n] = 1;
+        std::vector<int> pref(n + 1, 0);
         
-        int s = 0;
-        int ans = 0;
-        int cur = 0;
-
         for (int i = 0; i < n; ++i) {
-            if (nums[i] == target) {
-                cur += f[s + n];
-                s++;
-            } else {
-                s--;
-                cur -= f[s + n];
-            }
-            ans += cur;
-            f[s + n]++;
+            pref[i + 1] = pref[i] + (nums[i] == target ? 1 : -1);
         }
+        
+        return mergeSortAndCount(pref, 0, n);
+    }
 
-        return ans;
+private:
+    int mergeSortAndCount(std::vector<int>& pref, int left, int right) {
+        if (left >= right) return 0;
+        
+        int mid = left + (right - left) / 2;
+        int count = 0;
+        
+        count += mergeSortAndCount(pref, left, mid);
+        count += mergeSortAndCount(pref, mid + 1, right);
+        count += mergeAndCount(pref, left, mid, right);
+        
+        return count;
+    }
+    
+    int mergeAndCount(std::vector<int>& pref, int left, int mid, int right) {
+        int count = 0;
+        int j = mid + 1;
+        
+        for (int i = left; i <= mid; ++i) {
+            while (j <= right && pref[j] <= pref[i]) {
+                j++;
+            }
+            count += (right - j + 1);
+        }
+        
+        std::vector<int> temp;
+        int p1 = left, p2 = mid + 1;
+        while (p1 <= mid && p2 <= right) {
+            if (pref[p1] <= pref[p2]) {
+                temp.push_back(pref[p1++]);
+            } else {
+                temp.push_back(pref[p2++]);
+            }
+        }
+        while (p1 <= mid) 
+            temp.push_back(pref[p1++]);
+        while (p2 <= right) 
+            temp.push_back(pref[p2++]);
+        
+        for (int i = 0; i < temp.size(); ++i) {
+            pref[left + i] = temp[i];
+        }
+        
+        return count;
     }
 };
